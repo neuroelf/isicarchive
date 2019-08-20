8f57afdc37d87e617cfd028e04eaffcd27722730
neuroelf <cj.hammond.gm@gmail.com>
2019-08-20 17:05:32 -0400

    Updated notebook and test.

--- isicarchive/tests/__init__.py
+++ isicarchive/tests/__init__.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
-from isicarchive import IsicApi
+from isicarchive import IsicApi, func
 
 if __name__ == '__main__':
     
     # Create and print info about IsicApi object
     api = IsicApi(cache_folder='C:\\Users\\weberj3\\Documents\\ISIC')
     api.cache_images()
+    images = [v for v in api._image_cache.values()]
+    image_diagnoses = func.getxattr(images, '[].meta.clinical.diagnosis')
     
     # Load and print info about a study
     study = api.study('ISIC Annotation Study - All Features')
+    study.load_annotations()
+    study.cache_imagedata()
+    
+    # get annotation
     annotation = api.annotation(study.annotations[0])
     annotation._repr_pretty_()
     
-    study.load_images()
-    study.load_annotations()
-    study.loaded_features
-
     # Retrieve an image for this study
     image = api.image(study.images[0])
     
